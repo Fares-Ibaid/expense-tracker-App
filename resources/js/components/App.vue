ba71fe5166ad6a58d13512acbb174e21493b9fb9
Fares Ibaid <fares.ibaid@dittmeier.de>
2025-08-14 15:47:23 +0200

    Hallo World message from Tailwind

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 
 </script>
-
 <template>
-    <h1>Hello from Vue 3 👋</h1>
+    <div class="min-h-screen flex items-center justify-center bg-gray-100 text-2xl text-blue-600">
+        <h1>Hello from Vue 3 👋</h1>
+        <p>Tailwind is working 🎉 </p>
+    </div>
 </template>
 
 <style scoped>
