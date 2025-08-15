108c1cda5c282731d123248576626bd4cffdbb40
Fares Ibaid <fares.ibaid@dittmeier.de>
2025-08-15 23:10:16 +0200

    clean some makeup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Vue + Laravel</title>
+    <title>Expenses Tracker 2025</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
