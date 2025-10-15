a0ff38081e60a08d59ee73e43036a3c4c90fbef4
Fares Ibaid <fares.ibaid@dittmeier.de>
2025-10-15 15:03:56 +0200

    Fix searching by Category Bug
    replace has in the request with filled to ensure there is a value when query the database

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -32,32 +32,32 @@ class DashboardController extends Controller
            }
 
         // Filter by Date Range or Month/Year
-        if ($request->has('startDate') && $request->has('endDate')) {
+   if ($request->filled('startDate') && $request->filled('endDate')) {
             $filtersApplied = true;
             $query->whereBetween('date', [$request->query('startDate'), $request->query('endDate')]);
-      }  elseif ($request->has('month') || $request->has('year')) {
-            $filtersApplied = true;
-            $month = $request->query('month', null); // Null if not provided
-            $year = $request->query('year', date('Y')); // Default to current year if not provided
+         }elseif ($request->filled('month') || $request->filled('year')) {
+                $filtersApplied = true;
+                //dd($request->all());
+                $month = $request->query('month', null); // Null if not provided
+                $year = $request->query('year', date('Y')); // Default to current year if not provided
 
-            if ($month) {
-                // If month is provided, filter for the given month and year
-                $startOfMonth = date("Y-m-d", strtotime("$year-$month-01"));
-                $endOfMonth = date("Y-m-t", strtotime("$year-$month-01"));
-                $query->whereBetween('date', [$startOfMonth, $endOfMonth]);
-            } else {
-                // If only year is provided, filter for the entire year
-                $startOfYear = date("Y-01-01", strtotime("$year-01-01"));
-                $endOfYear = date("Y-12-31", strtotime("$year-01-01"));
-                $query->whereBetween('date', [$startOfYear, $endOfYear]);
-            }
+                if ($month) {
+                    // If month is provided, filter for the given month and year
+                    $startOfMonth = date("Y-m-d", strtotime("$year-$month-01"));
+                    $endOfMonth = date("Y-m-t", strtotime("$year-$month-01"));
+                    $query->whereBetween('date', [$startOfMonth, $endOfMonth]);
+                } else {
+                    // If only year is provided, filter for the entire year
+                    $startOfYear = date("Y-01-01", strtotime("$year-01-01"));
+                    $endOfYear = date("Y-12-31", strtotime("$year-01-01"));
+                    $query->whereBetween('date', [$startOfYear, $endOfYear]);
+                }
         }
         // filter by amount
-        if($request->has('min_amount') && $request->has('max_amount')) {
+        if ($request->filled('minAmount') && $request->filled('maxAmount')) {
             $filtersApplied = true;
-            $query->whereBetween('amount', [$request->query('min_amount'), $request->query('max_amount')]);
+            $query->whereBetween(DB::raw('ABS(amount)'), [$request->query('minAmount'), $request->query('maxAmount')]);
         }
-
         // pagination
         $perPage  = $request->query('per_page', 5); // this will be changed dynamically from the frontend
         $expenses = $query->paginate($perPage);
